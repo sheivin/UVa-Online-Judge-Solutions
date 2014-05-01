96ab1a7aa7dc114f1033595a31a28a21fcc22535
Sheivin Goyal <sheivin@Sheivins-MacBook-Pro.local>
2014-05-02 00:24:36 +0530

    My solution..

---
+++ Problem_10071.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Problem_10071
+{
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String args[])
+    {
+        int v=0;
+        int t=0;
+        while(sc.hasNext())
+        {
+            v=sc.nextInt();
+            t=sc.nextInt();
+            System.out.println(2*v*t);
+        }
+    }
+}
